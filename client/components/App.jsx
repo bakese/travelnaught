19f09d7ed989ea2b0991a0ac99f5336580ee73c9
bakese <sethlbaker09@gmail.com>
2021-08-24 13:32:01 -0400

    base requests are working to the api

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 
 const App = () => {
 
+  const searchFlight = () => {
+    axios.get('/flights')
+    .then((response) => console.log('success'))
+    .catch((err) => console.error(err))
+  }
 
   return (
     <div>
-      <h1>App.js is connected and working!</h1>
+      <h1>Travelnaughts</h1>
+      <button onClick={searchFlight}>Search flight</button>
     </div>
   )
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,87 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const port = 3005;
+const Duffel = require('@duffel/api')
+const token = require('../config.js')
+const axios = require('axios')
 
 app.use(express.static(path.join(__dirname, '..')));
 
 app.listen(port, () => {
   console.log(`Server listening at localhost:${port}!`);
 });
+
+
+app.get('/flights', (req, res) => {
+  var data = JSON.stringify({
+    "data": {
+      "slices": [
+        {
+          "origin": "NYC",
+          "destination": "ATL",
+          "departure_date": "2021-10-21"
+        },
+        {
+          "origin": "ATL",
+          "destination": "NYC",
+          "departure_date": "2021-10-29"
+        }
+      ],
+      "passengers": [
+        {
+          "type": "adult"
+        }
+      ],
+      "cabin_class": "economy"
+    }
+  });
+
+  var config = {
+    method: 'post',
+    url: 'https://api.duffel.com/air/offer_requests',
+    headers: {
+      'Accept-Encoding': 'gzip',
+      'Accept': 'application/json',
+      'Duffel-Version': 'beta',
+      'Authorization': token,
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+
+  axios(config)
+  .then(function (response) {
+    // console.log(JSON.stringify(response.data));
+    res.send(response.data.data)
+  })
+  .catch(function (error) {
+    // console.log(error);
+    res.send(error)
+  });
+})
+
+
+app.get('/offers', (req, res) => {
+  var config = {
+    method: 'get',
+    url: 'https://api.duffel.com/air/offers/',
+    headers: {
+      'Accept-Encoding': 'gzip',
+      'Accept': 'application/json',
+      'Duffel-Version': 'beta',
+      'Authorization': token
+    }
+  };
+
+  axios(config)
+  .then(function (response) {
+    // console.log(JSON.stringify(response.data));
+    res.send(response.data)
+
+  })
+  .catch(function (error) {
+    // console.log(error);
+    res.send(error)
+  });
+})
